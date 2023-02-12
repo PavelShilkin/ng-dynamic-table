96351d1e2d62fca3c1004caa9c4033dff616e7a4
pshilkin <pshilkin@htsts.ru>
2023-02-12 17:46:31 +0300

    Добавлены кнопки для редактирования таблицы

--- src/app/core/dynamic-table.module.ts
+++ src/app/core/dynamic-table.module.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { ButtonModule } from "primeng/button";
 import { TableModule } from "primeng/table";
+import { TooltipModule } from "primeng/tooltip";
 import { DynamicTableComponent } from "./components/dynamic-table/dynamic-table.component";
 import { DynamicTablePipe } from "./pipes/dynamic-table-value.pipe";
 
-const PRIMENG_MODULES = [TableModule];
+const PRIMENG_MODULES = [TableModule, ButtonModule];
 
 @NgModule({
   declarations: [DynamicTableComponent, DynamicTablePipe],
-  imports: [CommonModule, ...PRIMENG_MODULES],
+  imports: [CommonModule, TooltipModule, ...PRIMENG_MODULES],
   exports: [DynamicTableComponent],
   providers: [],
 })
